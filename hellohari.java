bb94f6a963be40f85978142ccfc3f76ebcfa0720
HarisGit <0.harikrishnaaws@gmail.com>
2018-06-20 10:58:28 -0400

    changed printing text

--- hellohari.java
+++ hellohari.java
@@ -3,7 +3,7 @@ public class hellohari
 	public static void main (String[] args)
 	{
 		for (int i=1;i<=5;i++)
-		{ System.out.println("Hey this is Hari.."+i);
+		{ System.out.println("Hey welcome to Devops world.."+i);
 		}
 	}
 }
